2c769f8199b63f91e10069024b6f81329000fb05
qucals <totalboy@outlook.com>
2020-03-08 13:29:26 +0500

    Added .gitignore & Removed useless files

--- LibraryViewController 2.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LibraryViewController.swift
-//  SmartLibrary
-//
-//  Created by Кирилл Галимзянов on 27.02.2020.
-//  Copyright © 2020 Kirill Galimzyanov. All rights reserved.
-//
-
-import Foundation
