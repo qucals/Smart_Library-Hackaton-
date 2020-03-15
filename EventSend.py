75bfd2477cb527aca58972ebf76f19166ae4b9a9
Soblev515 <alex.sobolev515@yandex.ru>
2020-03-15 10:44:59 +0500

    Update EventSend.py

--- EventSend.py
+++ EventSend.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from datetime import datetime, date, time
 
-cred = credentials.Certificate('C:/Users/Влад/Downloads/test-dfa5f-firebase-adminsdk-i65lc-31ead13503.json')
+cred = credentials.Certificate('C:/Users/itcub/Documents/GitHub/iOS_Library/smart-library-8a179-firebase-adminsdk-c01sz-f51272754e.json')
 
 # Подключаемся к  БД
 firebase_admin.initialize_app(cred)
@@ -11,26 +11,30 @@ firebase_admin.initialize_app(cred)
 # Подключаемся
 db = firestore.client()
 
-docs = db.collection(u'events').stream()
-
 #Вводим информация
-print('Enter name')
-name = input()
-print("Format date: DD-MM-YYYY HH:MM")
-print("Enter date:")
-fdate = input()
-date = datetime.strptime(fdate, "%d-%m-%Y %H:%M")
-print('Add description')
-description = input()
-doc = db.collection(u'events').document(u'first event').get().to_dict()
-print(doc['date'])
+#print('Enter name')
+#name = input()
+#print("Format date: DD-MM-YYYY HH:MM")
+#print("Enter date:")
+#fdate = input()
+#date = datetime.strptime(fdate, "%d-%m-%Y %H:%M")
+#print('Add description:')
+#description = input()
+#print('place:')
+#place = input()
+#print('communication:')
+#communication = input()
 
-def To_send():
-    datetime.now()
-    doc_ref = db.collection(u'event').document('ADA')
-    doc_ref.set({
-        u'name': name,        
-        u'description': '',
-        u'date': date
-    })
-To_send()
+# отправка данных
+def To_send(name, description, date, place, communication):
+    data = {
+        u'name': name, 
+        u'description': description,
+        u'date': date,
+        u'place': place,
+        u'communication': communication
+    }
+    db.collection(u'events').add(data)
+
+
+# To_send(name, description, date, place, communication)
